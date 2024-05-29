6d52bc6ac72ab3dff43d86189a401094a1809e3f
ElvisMw <elvismwanthi@gmail.com>
2024-05-29 20:04:10 +0300

    2-app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Create a get_locale function with the babel.localeselector decorator.
+Use request.accept_languages to determine the
+best match with our supported languages.
+"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
