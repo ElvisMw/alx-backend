270d1ee58ce57bdec9bb45882bb91978ecf05dfd
ElvisMw <elvismwanthi@gmail.com>
2024-05-29 19:56:07 +0300

    2-app

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """
+    Route to render the index page.
+    """
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Function to get the best-matched locale based on the request's
+    Accept-Language header.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
