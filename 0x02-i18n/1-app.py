afc48e51b90ac1ac38097ffb96ba21e8807ce894
ElvisMw <elvismwanthi@gmail.com>
2024-05-29 17:13:42 +0300

    1-app

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request
+
+"""
+This script defines a Flask application and sets up Babel for
+internationalization.
+"""
+
+from flask import Flask, render_template
 from flask_babel import Babel
 
+# Create a Flask application
 app = Flask(__name__)
+
+# Set the default locale and timezone for Babel
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
+
+# Set the languages to support
 app.config['LANGUAGES'] = ['en', 'fr']
 
+# Initialize Babel
 babel = Babel(app)
 
 
 @app.route('/')
 def index():
+    """
+    This function handles the root route and renders the index.html template.
+
+    Returns:
+        The rendered index.html template.
+    """
     return render_template('1-index.html')
 
 
 if __name__ == "__main__":
+    # Run the Flask application
     app.run()
