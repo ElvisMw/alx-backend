b0fc6651978aa8051611c125163dabc77874fa60
ElvisMw <elvismwanthi@gmail.com>
2024-05-31 16:28:17 +0300

    4-app

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+"""
+Flask web application that uses Flask-Babel for internationalization.
+"""
+
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 
 class Config:
+    """
+    Configuration class for Flask app.
+    """
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -15,6 +22,13 @@ babel = Babel(app)
 
 
 def get_locale():
+    """
+    Function to get the best-matched locale based on the request's
+    Accept-Language header and URL parameters.
+
+    Returns:
+        str: The best-matched locale.
+    """
     # Check for a locale argument in the URL parameters
     locale = request.args.get('locale')
     if locale in app.config['LANGUAGES']:
@@ -27,8 +41,15 @@ babel = Babel(app, locale_selector=get_locale)
 
 @app.route('/')
 def index():
+    """
+    Route to render the index page.
+
+    Returns:
+        The rendered index page.
+    """
     return render_template('4-index.html')
 
 
 if __name__ == '__main__':
+    # Run the application in debug mode
     app.run(debug=True)
