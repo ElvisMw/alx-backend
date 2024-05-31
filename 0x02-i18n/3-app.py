3a03463ab6f676da54912485cac24cc763148443
ElvisMw <elvismwanthi@gmail.com>
2024-05-31 16:09:40 +0300

    3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,42 +1,24 @@
 #!/usr/bin/env python3
-
-"""
-Main application module for the Flask app.
-"""
-
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
+class Config:
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 app = Flask(__name__)
-
-# Configuring the application
-app.config['BABEL_DEFAULT_LOCALE'] = 'en'  # Default locale
-app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'  # Default timezone
-app.config['LANGUAGES'] = ['en', 'fr']  # Supported languages
-
-# Initializing Babel
+app.config.from_object(Config)
 babel = Babel(app)
 
-
 def get_locale():
-    """
-    Function to get the best-matched locale based on the request's
-    Accept-Language header.
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.args.get('locale') or request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
-babel.init_app(app, locale_selector=get_locale)
-
+babel = Babel(app, locale_selector=get_locale)
 
 @app.route('/')
 def index():
-    """
-    Route to render the index page.
-    """
     return render_template('3-index.html')
 
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
